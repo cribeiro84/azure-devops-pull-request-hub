1e0f1d5edf0effe97fdab85e9907dc3e9499249e
Vlad Kasianenko <Vladyslav_Kasianenko@rcomext.com>
2020-07-03 20:29:49 +0300

    fix: it's ok when more people approved

--- src/tabs/PulRequestsTabData.tsx
+++ src/tabs/PulRequestsTabData.tsx
@@ -614,7 +614,7 @@ export function getPullRequestPolicyAsync(
                     }
 
                     pullRequestPolicy.isReviewersApprovedOk =
-                      reviewerCount === pullRequestPolicy.minimumApproverCount;
+                      reviewerCount >== pullRequestPolicy.minimumApproverCount;
                     pullRequestPolicy.reviewerCount = reviewerCount;
                   } else if (
                     pullRequestPolicy.displayName === "Work item linking"
